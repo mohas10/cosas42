59a8bed47213415154cef8d76369b34363841d5c
mohas10 <115185316+mohas10@users.noreply.github.com>
2022-10-06 22:33:17 +0200

    Add files via upload

---
+++ libft/libft.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: msagna <msagna@student.42barcelon>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/29 21:46:55 by msagna            #+#    #+#             */
+/*   Updated: 2022/09/29 21:57:08 by msagna           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#ifndef LIBFT.H
+#define LIBFT.H
+
+int	ft_isalnum(int);
+int	ft_isalpha(int);
+int	ft_isascii(int);
+int	ft_isdigit(int);
+int	ft_isprint(int);
+int	ft_strlen(char);
+
+#endif
