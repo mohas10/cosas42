5e6ef975b5e4e69740f7110935c17388d91e1df5
Mohamadou Sagna <msagna@cbr13s6.42barcelona.com>
2022-10-13 22:16:53 +0200

    empezado bzero finikitao el memset

--- libft/ft_bzero.c
+++ libft/ft_bzero.c
@@ -1,28 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*   ft_bzero.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: msagna <msagna@student.42barcelon>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/06 21:09:55 by msagna            #+#    #+#             */
-/*   Updated: 2022/10/13 22:14:39 by msagna           ###   ########.fr       */
+/*   Created: 2022/10/13 21:56:44 by msagna            #+#    #+#             */
+/*   Updated: 2022/10/13 22:16:11 by msagna           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include "libft.h"
-#include <stddef.h> //para el size_t
-void	*ft_memset(void *b, int c, size_t len)
-{
-	int	i;
-	unsigned char	c2;
-	unsigned char*	b2;
 
-	i = 0;
-	c2 = c;
-	b2 = b;
-	while (i < len)
-	{
-		b2[i] = c2;
-		i++;
-	}
-}
+
